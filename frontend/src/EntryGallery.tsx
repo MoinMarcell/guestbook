10681717dc652ef6d8adacff0b7b48bfdd73bd9d
MoinMarcell <marcell.dechant@proton.me>
2023-03-02 18:56:39 +0100

    Add handling for login & register button

--- frontend/src/EntryGallery.tsx
+++ frontend/src/EntryGallery.tsx
@@ -1,23 +1,36 @@
 import {GuestbookEntry} from "./GuestbookEntry";
 import EntryCard from "./EntryCard";
 import AddEntry from "./AddEntry";
+import {useNavigate} from "react-router-dom";
 
 type EntryGalleryProps = {
     entries: GuestbookEntry[],
+    token: string | null,
 }
 
-export default function EntryGallery(props: EntryGalleryProps){
+export default function EntryGallery(props: EntryGalleryProps) {
+
+    const navigate = useNavigate();
+
     const entryCard = props.entries.map((entry) => {
         return (
             <EntryCard entry={entry} key={entry.id}/>
         );
     }).reverse();
 
+    function handleRegisterClick() {
+        navigate("/register");
+    }
+
+    function handleLoginClick() {
+        navigate("/login");
+    }
+
     return (
         <div>
-            <button>Register</button>
-            <button>Login</button>
-            <AddEntry/>
+            <button onClick={handleRegisterClick}>Register</button>
+            <button onClick={handleLoginClick}>Login</button>
+            <AddEntry token={props.token}/>
             {entryCard}
         </div>
     );
